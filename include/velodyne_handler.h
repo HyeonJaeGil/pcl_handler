82769217fc7fb753a46ad9b2015abfc4be3155c2
hyeonjaegil <now9728@gmail.com>
2021-12-03 20:32:46 +0900

    add velodyneHandler base class

---
+++ include/velodyne_handler.h
@@ -0,0 +1,25 @@
+#include <ros/ros.h>
+#include <iostream>
+#include <sensor_msgs/PointCloud2.h>
+
+
+class velodyneHandler
+{
+public:
+    velodyneHandler();
+    velodyneHandler(std::string in_topic, std::string out_topic)
+        :cloud_in_topic_(in_topic), cloud_out_topic_(out_topic) {};
+    // virtual ~velodyneHandler();
+    std::string cloud_in_topic_;
+    std::string cloud_out_topic_;
+    ros::Publisher pub_;
+    ros::Subscriber sub_;
+    void cloud_cb(const boost::shared_ptr<const sensor_msgs::PointCloud2> in_cloud);
+
+protected:
+    ros::NodeHandle nh_;
+
+private:
+
+};
+
